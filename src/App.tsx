46479b2b7443227c3b5a5427a6f9a3bf6266ac75
dolsev <dolod777@yandex.ru>
2023-03-19 23:08:56 +0300

    all is working properly, trying some twicks

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import store from './store';
+import { loadCharacters, addCharacter, removeCharacter, likeCharacter } from './store';
 import { AppState } from './store';
 import { Character } from './interfaces/Character';
 import './App.css';
@@ -9,50 +9,68 @@ function App() {
     const dispatch = useDispatch();
     const characters = useSelector((state: AppState) => state.characters);
 
+    const [showLikes, setShowLikes] = useState(false);
+
     useEffect(() => {
         const charactersFromLocalStorage: Character[] = JSON.parse(localStorage.getItem('characters') || '[]');
         if (charactersFromLocalStorage.length > 0) {
-            charactersFromLocalStorage.forEach((character: Character) => {
-                store.dispatch({ type: 'ADD_CHARACTER', payload: character });
-            });
-        } else {
+            dispatch(loadCharacters(charactersFromLocalStorage));
+        }
+        else {
             fetch('https://hp-api.onrender.com/api/characters')
                 .then((response) => response.json())
                 .then((data) => {
-                    // Add the first 25 characters to our store
                     data.slice(0, 25).forEach((character: Character) => {
-                        store.dispatch({ type: 'ADD_CHARACTER', payload: character });
+                        dispatch(addCharacter(character));
                     });
                     localStorage.setItem('characters', JSON.stringify(data.slice(0, 25)));
                 });
         }
-    }, []);
+    }, [dispatch]);
 
     const handleDelete = (character: Character) => {
         console.log('Deleting character:', character);
-        dispatch({ type: 'REMOVE_CHARACTER', payload: character });
-    };    const handleLike = (character: Character) => {
-        dispatch({ type: 'LIKE_ACTION', payload: character });
+        dispatch(removeCharacter(character));
     };
 
+    const handleLike = (character: Character) => {
+        dispatch(likeCharacter(character));
+    };
+
+    const handleRefresh = () => {
+        localStorage.removeItem('characters');
+        window.location.reload();
+    };
+
+    const filteredCharacters = showLikes ? characters.filter((character: Character) => character.liked) : characters;
 
     return (
-        <div className='card-list'>
-            {characters.map((character: Character) => (
-                <div className='card' key={character.id + '_singleCard'}>
-                    <h2>{character.name}</h2>
-                    <div className='avatar-div'>
-                        <img className='avatar' src={character.image} alt={character.name} />
+        <div className='app'>
+            <nav>
+                <button onClick={handleRefresh}>Refresh</button>
+                <button onClick={() => setShowLikes(!showLikes)}>
+                    {showLikes ? 'Show All' : 'Filter by Likes'}
+                </button>
+            </nav>
+            <div className='card-list'>
+                {filteredCharacters.map((character: Character) => (
+                    <div className='card' key={character.id + '_singleCard'}>
+                        <h2>{character.name}</h2>
+                        <div className='avatar-div'>
+                            <img className='avatar' src={character.image} alt={character.name} />
+                        </div>
+                        {character.house ? <h3>{character.house}</h3> : <h3>Homeless</h3>}
+                        <button className='like-button' onClick={() => handleLike(character)}>
+                            {character.liked ? 'Unlike' : 'Like'}
+                        </button>
+                        <button className='delete-button' onClick={() => handleDelete(character)}>
+                            Delete
+                        </button>
                     </div>
-                    {character.house ? <h3>{character.house}</h3> : <h3>Homeless</h3>}
-                    <button onClick={() => handleDelete(character)}>Delete</button>
-                    <button onClick={() => handleLike(character)}>
-                        {character.liked ? 'Unlike' : 'Like'}
-                    </button>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
     );
 }
 
-export default App
+export default App;
--- src/store.ts
+++ src/store.ts
@@ -1,88 +1,58 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { Character } from './interfaces/Character';
 
 interface AppState {
     characters: Character[];
 }
 
-interface AddCharacterAction {
-    type: 'ADD_CHARACTER';
-    payload: Character;
-}
-
-interface RemoveCharacterAction {
-    type: 'REMOVE_CHARACTER';
-    payload: Character;
-}
-
-interface LikeCharacterAction {
-    type: 'LIKE_ACTION';
-    payload: Character;
-}
-
-type AppAction = AddCharacterAction | RemoveCharacterAction | LikeCharacterAction | { type: 'LOAD_CHARACTERS', payload: Character[] };
-
 const initialState: AppState = {
     characters: [],
 };
 
-function appReducer(state = initialState, action: AppAction): AppState {
-    switch (action.type) {
-        case 'ADD_CHARACTER':
+export const characterSlice = createSlice({
+    name: 'characters',
+    initialState,
+    reducers: {
+        addCharacter: (state, action: PayloadAction<Character>) => {
             const characterToAdd = {
                 ...action.payload,
                 liked: false,
             };
-            return {
-                ...state,
-                characters: [...state.characters, characterToAdd],
-            };
-        case 'REMOVE_CHARACTER':
+            state.characters = [...state.characters, characterToAdd];
+            localStorage.setItem('characters', JSON.stringify(state.characters));
+        },
+        removeCharacter: (state, action: PayloadAction<Character>) => {
             const updatedCharacters = state.characters.filter(
-                (character) => character !== action.payload
+                (character) => character.id !== action.payload.id
             );
-            localStorage.setItem('characters', JSON.stringify(updatedCharacters));
-            return {
-                ...state,
-                characters: updatedCharacters,
-            };
-        case 'LIKE_ACTION':
-            const newCharacters = state.characters.map((character) => {
+            state.characters = updatedCharacters;
+            localStorage.setItem('characters', JSON.stringify(state.characters));
+        },
+
+        likeCharacter: (state, action: PayloadAction<Character>) => {
+            const updatedCharacters = state.characters.map((character) => {
                 if (character.id === action.payload.id) {
                     const updatedCharacter = {
                         ...character,
                         liked: !character.liked,
                     };
-                    const updatedCharacters = state.characters.map((char) => {
-                        if (char.id === updatedCharacter.id) {
-                            return updatedCharacter;
-                        }
-                        return char;
-                    });
-                    localStorage.setItem('characters', JSON.stringify(updatedCharacters));
                     return updatedCharacter;
                 }
                 return character;
             });
-            return {
-                ...state,
-                characters: newCharacters,
-            };
-        case 'LOAD_CHARACTERS':
-            return {
-                ...state,
-                characters: action.payload,
-            };
-        default:
-            return state;
-    }
-}
+            state.characters = updatedCharacters;
+            localStorage.setItem('characters', JSON.stringify(state.characters));
+        },
+        loadCharacters: (state, action: PayloadAction<Character[]>) => {
+            state.characters = action.payload;
+        },
+    },
+});
 
-
-
+export const { addCharacter, removeCharacter, likeCharacter, loadCharacters } = characterSlice.actions;
 
 const store = configureStore({
-    reducer: appReducer,
+    reducer: characterSlice.reducer,
 });
 
 export default store;
